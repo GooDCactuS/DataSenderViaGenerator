57535918f5a1b48350a7ed6edd0c4fee247f3ab3
GooDCactuS <marvele98@gmail.com>
2021-03-02 11:44:36 +0300

    feat: add encoder page

---
+++ DataSenderViaGenerator/Pages/Encoder.cshtml.cs
@@ -0,0 +1,16 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace DataSenderViaGenerator.Pages
+{
+    public class EncoderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
